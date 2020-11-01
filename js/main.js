6d9e61fb5aa03dcbf3953a3fa8412452b672dc96
unknown <marwashindi@gmail.com>
2020-11-01 16:09:25 +0200

    product-info page

--- js/main.js
+++ js/main.js
@@ -27,8 +27,28 @@ $(document).ready(function() {
         margin: 0,
         singleItem: true,
 
-    })
+    });
 
+    $('.option-carousel').owlCarousel({
+        rtl: true,
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        items: 1,
+        margin: 0,
+        singleItem: true,
+
+    });
+
+
+
+    // $(".box-link").click(function() {
+    //     //  e.preventDefault();
+    //     // open loading pic
+    //     $('.loader').addClass('apper');
+
+    // });
 
 });
 // =================================================================================// 
@@ -42,3 +62,80 @@ function cot() {
     count += 1;
     document.getElementById('cart-number').innerHTML = count;
 }
+// =================================================================================// 
+
+// Increment And Decrement Counter on Button Click 
+document.querySelector('.minus').setAttribute('disabled', 'disabled');
+
+// plus button
+var valueCount;
+// talking price value in variable
+var price = document.getElementById('price').innerText;
+
+// price calculation function
+function priceTotal() {
+    var total = valueCount * price;
+    document.getElementById('price').innerText = total;
+}
+
+document.querySelector('.plus').addEventListener('click', () => {
+    // geting value of input 
+    valueCount = document.getElementById('quantity-value').value;
+
+    // input value increment by 1
+    valueCount++;
+
+    // setting increment input value
+    document.getElementById('quantity-value').value = valueCount;
+
+    if (valueCount > 1) {
+        document.querySelector('.minus').removeAttribute('disabled');
+        document.querySelector('.minus').classList.remove('disabled');
+    }
+
+    // calling price function 
+    priceTotal();
+})
+
+// minus button
+
+var valueCount;
+
+document.querySelector('.minus').addEventListener('click', () => {
+    // geting value of input 
+    valueCount = document.getElementById('quantity-value').value;
+
+    // input value decrement by 1
+    valueCount--;
+
+    // setting decrement input value
+    document.getElementById('quantity-value').value = valueCount;
+
+    if (valueCount == 1) {
+        document.querySelector('.minus').setAttribute('disabled', 'disabled');
+    }
+    // calling price function 
+    priceTotal()
+})
+
+
+
+// Add loader when click on box-link 
+// var link = document.querySelectorAll('.box-link img');
+// var loader = document.querySelectorAll('.loader');
+
+// link.forEach((el) => {
+//     el.addEventListener('click', (e) => {
+//         // loader.className += ' apper';
+//         e.target.classList.add('apper')
+
+//     })
+// });
+
+
+
+
+
+// function load() {
+//     loader.className += ' apper';
+// }
